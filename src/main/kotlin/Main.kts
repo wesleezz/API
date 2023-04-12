b88ae18da7acaf9475d226d13ab747f1bdf37dfd
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-04-12 20:32:45 +0200

    🕊️ Подробнее

--- src/main/kotlin/Main.kts
+++ src/main/kotlin/Main.kts
@@ -1,7 +1,12 @@
-fun main(args : Array<String>) {
-print("Введите имя")
-
+fun main(args: Array<String>) {
+    println("Введите имя")
     val stringInput = readLine()!!
-    println("Ваше имя $stringInput")
+    println("\nВаше имя $stringInput")
     println("Добро пожаловать в NekoWorlds API!")
+    
+    println("Нажмите enter")
+    readLine() // ждём нажатия enter
+    
+    // Подробнее
+    println("\nЭто API которое пока-что бесполезно\nи я незнаю зачем я его создал\nно надеюсь в будущеем это будет полезно")
 }
