ac4c4668783707c75e6e5df40ce388fb078ea4f9
Wesleezz <72977084+wesleezz@users.noreply.github.com>
2023-04-12 14:59:15 +0200

    Create Main.kts

---
+++ src/main/kotlin/Main.kts
@@ -0,0 +1,7 @@
+fun main(args : Array<String>) {
+print("Введите имя")
+
+    val stringInput = readLine()!!
+    println("Ваше имя $stringInput")
+    println("Добро пожаловать в NekoWorlds API!")
+}
